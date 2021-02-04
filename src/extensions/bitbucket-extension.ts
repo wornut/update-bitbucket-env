cf8096b7c15cabbb72051893d30d489538ed7ee7
worasitipon <worasitipon.t@hotmail.com>
2021-02-04 19:43:46 +0700

    update bitbucket selection

--- src/extensions/bitbucket-extension.ts
+++ src/extensions/bitbucket-extension.ts
@@ -1,5 +1,6 @@
 import { GluegunToolbox } from 'gluegun'
 import { APIClient, Bitbucket } from 'bitbucket'
+// import { ArrayPromptOptions } from 'gluegun/build/types/toolbox/prompt-enquirer-types'
 
 // side effect
 export let client: APIClient
@@ -9,13 +10,57 @@ export interface IAuth {
   password: string
 }
 
-export function getBitbucketClient(auth: IAuth) {
-  return new Bitbucket({
-    auth,
-    request: {
-      timeout: 20000
-    }
+export async function selectRepos({ prompt, print }: GluegunToolbox) {
+  const { data: workspaceResult } = await client.workspaces.getWorkspaces({
+    fields: 'values.slug'
   })
+
+  const { workspace } = await prompt.ask({
+    type: 'select',
+    name: 'workspace',
+    message: 'select bitbucket workspace (slug)',
+    choices: workspaceResult.values.map(({ slug }) => slug)
+  })
+
+  const { data: projectResult } = await client.workspaces.getProjects({
+    workspace,
+    fields: 'values.name,values.key'
+  })
+
+  const { project } = await prompt.ask({
+    type: 'select',
+    name: 'project',
+    message: 'select bitbucket workspace project',
+    choices: projectResult.values.map(({ key }) => key)
+  })
+
+  const { data: repoResult } = await client.repositories.list({
+    workspace,
+    fields: 'values.slug,values.name,values.uuid,values.project.key',
+    q: `project.key = "${project}"`
+  })
+
+  // const repoQuestion: ArrayPromptOptions = {
+  //   type: 'muliselect',
+  //   name: 'repos',
+  //   message: 'select bitbucket workspace project',
+  //   choices: repoResult.values.map(({ name, uuid }) => ({ name, value: uuid }))
+  // }
+
+  const { repos } = await prompt.ask({
+    type: 'select',
+    multiple: true,
+    scroll: true,
+    name: 'repos',
+    message: 'select bitbucket workspace project',
+    choices: repoResult.values.map(({ name, uuid }) => ({
+      name,
+      message: name,
+      value: uuid
+    }))
+  })
+
+  print.debug(repos)
 }
 
 export default (toolbox: GluegunToolbox) => {
@@ -23,6 +68,19 @@ export default (toolbox: GluegunToolbox) => {
     const { username, password } = await toolbox.loadBitbucketCredential(
       toolbox
     )
-    client = getBitbucketClient({ username, password })
+
+    // override bitbucket instance
+    client = new Bitbucket({
+      auth: {
+        username,
+        password
+      },
+      request: {
+        timeout: 20000
+      }
+    })
+
+    await selectRepos(toolbox)
+    // await getRelateRepos(toolbox, workspace)
   }
 }
