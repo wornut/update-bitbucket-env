77a54824a67666c25b5bf095fd6bd48a81c41c3c
worasitipon <worasitipon.t@hotmail.com>
2021-02-04 18:13:58 +0700

    update bitbucket extension

--- src/extensions/bitbucket-extension.ts
+++ src/extensions/bitbucket-extension.ts
@@ -1,11 +1,10 @@
-import { GluegunToolbox, print } from 'gluegun'
+import { GluegunToolbox } from 'gluegun'
 import { APIClient, Bitbucket } from 'bitbucket'
-import { isNil } from 'lodash'
 
 // side effect
 export let client: APIClient
 
-interface IAuth {
+export interface IAuth {
   username: string
   password: string
 }
@@ -19,53 +18,11 @@ export function getBitbucketClient(auth: IAuth) {
   })
 }
 
-export async function testBitbucketCredential(auth: IAuth) {
-  await getBitbucketClient(auth)
-    .user.get({})
-    .then(({ data }) => {
-      if (isNil(data)) throw Error('can not access with ' + auth)
-      else print.success(data)
-    })
-    .catch(print.error)
-}
-
-export async function isClientGood(toolbox: GluegunToolbox): Promise<boolean> {
-  if (!isNil(client)) return true
-
-  if (isNil(toolbox.config.credential)) {
-    const { username, password } = await toolbox.prompt.ask([
-      {
-        type: 'input',
-        name: 'username',
-        message: 'enter bitbucket username'
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'enter bitbucket password (stdin supported)'
-      }
-    ])
-
-    Reflect.set(toolbox.config, 'credential', {
-      bitbucket: { username, password }
-    })
-
-    toolbox.print.debug(toolbox.config.credential)
-
-    //    client = getBitbucketClient({ username: String(username), password: String(password) })
-
-    await testBitbucketCredential({
-      username: String(username),
-      password: String(password)
-    })
-
-    return true
-  }
-
-  return false
-}
-
 export default (toolbox: GluegunToolbox) => {
-  toolbox.isClientGood = isClientGood
-  toolbox.testBitbucketCredential = testBitbucketCredential
+  toolbox.setupBitbucketCredential = async () => {
+    const { username, password } = await toolbox.loadBitbucketCredential(
+      toolbox
+    )
+    client = getBitbucketClient({ username, password })
+  }
 }
